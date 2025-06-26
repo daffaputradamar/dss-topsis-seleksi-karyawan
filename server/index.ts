c2d582056bbf91099bb3e82251fd483f8a3797ea
Daffa Akbar <daffaputradamar@gmail.com>
2025-06-26 23:59:20 +0700

    fix: Use environment variable for server port configuration

--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = process.env.PORT || 5000;
   server.listen({
     port
   }, () => {
